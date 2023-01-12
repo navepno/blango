5a3cd16ca5c0b47522873ace6b4eed33282a47ae
Zhrakov Anton <navepno@gmail.com>
2023-01-12 15:54:43 +0000

    working with authentication

--- blango/settings.py
+++ blango/settings.py
@@ -63,6 +63,7 @@ class Dev(Configuration):
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
       'rest_framework',
+      'rest_framework.authtoken',
   ]
 
   MIDDLEWARE = [
@@ -217,6 +218,14 @@ class Dev(Configuration):
   ACCOUNT_EMAIL_REQUIRED = True
   ACCOUNT_USERNAME_REQUIRED = False
   ACCOUNT_AUTHENTICATION_METHOD = "email" 
+
+  REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
   
 class Prod(Dev):
