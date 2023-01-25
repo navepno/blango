5e1577262e3a2af88481ed517517dbce127fd2d7
Zhrakov Anton <navepno@gmail.com>
2023-01-25 14:02:24 +0000

    working with related fields

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -7,6 +7,7 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
 ]
